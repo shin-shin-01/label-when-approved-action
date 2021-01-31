a88208dbfe1b643faa3bd887c335ce9f4367ff2d
Kaze Shindo <kaze.bknb@outlook.jp>
2021-01-31 13:03:28 +0900

    npm run format

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-export function main(): void{
+export function main(): void {
   try {
-    const nameToGreet: string = core.getInput('who-to-greet')
+    const nameToGreet: string = core.getInput("who-to-greet");
     console.log(`Hello ${nameToGreet}!`);
-    const time: string = (new Date()).toTimeString();
+    const time: string = new Date().toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload: string = JSON.stringify(github.context.payload, undefined, 2)
+    const payload: string = JSON.stringify(
+      github.context.payload,
+      undefined,
+      2
+    );
     console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
