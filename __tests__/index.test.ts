45e6161ae88cb006be94503fe73036d6441410be
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-02 23:18:07 +0900

    create test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -22,80 +22,117 @@ describe('mockInput', () => {
 
 test('succeess: label to pr', async () => {
   // @ts-ignore
+  // ある程度必要な情報を定義
   github.context = {
     payload: {
-      action: 'opened',
-      number: '1',
+      action: "submitted",
       pull_request: {
-        labels: [],
-        number: 1,
-        title: 'test',
-        user: {
-          login: 'pr-creator',
+        assignee: null,
+        assignees: [],
+        auto_merge: null,
+        base: {
+          ref: "master",
         },
+        closed_at: null,
+        draft: false,
+        labels: ["labeled"],
       },
-      repository: {
-        name: 'label-when-approved-action',
-        owner: {
-          login: 'shin-shin-01',
-        },
+      review: {
+        state: "approved",
       },
     },
-    issue: {
-      owner: 'label-when-approved-action',
-      repo: 'shin-shin-01',
-      number: 1,
-    },
+    eventName: "pull_request_review",
+    actor: "Kaze-for-test",
   }
   /* 
   client = github.getOctokit(myToken) より client: undefined になる
   addLabels: client.issues.addLabels ->  エラーが発生するため
   モックとして作成し，Promise<void>を返すようにしておく
   */
-  const addLabelsSpy = jest.spyOn(PullRequest.prototype, 'addLabels').mockReturnValue(Promise.resolve());
-  const hasAnyLabelSpy = jest.spyOn(PullRequest.prototype, 'hasAnyLabel')
+ const isApprovedSpy = jest.spyOn(PullRequest.prototype, 'isApproved')
+ const getUserNameSpy = jest.spyOn(PullRequest.prototype, 'getUserName')
+ const hasAnyLabelSpy = jest.spyOn(PullRequest.prototype, 'hasAnyLabel')
+ const addLabelsSpy = jest.spyOn(PullRequest.prototype, 'addLabels').mockReturnValue(Promise.resolve());
 
-  await main()
-  expect(hasAnyLabelSpy).toHaveBeenCalled()
-  expect(addLabelsSpy).toHaveBeenCalled()
+ await main()
+ expect(isApprovedSpy).toHaveBeenCalled()
+ expect(getUserNameSpy).toHaveBeenCalled()
+ expect(hasAnyLabelSpy).toHaveBeenCalled()
+ expect(addLabelsSpy).toHaveBeenCalled()
+ // ユーザ名の確認
+ expect(new PullRequest('token', github.context).getUserName()).toBe("Kaze-for-test")
 })
   
-test('failuer: already labeled', async () => {
+test('failuer: review is not APPROVED', async () => {
   // @ts-ignore
   github.context = {
     payload: {
-      action: 'opened',
-      number: '1',
+      action: "submitted",
       pull_request: {
+        assignee: null,
+        assignees: [],
+        auto_merge: null,
+        base: {
+          ref: "master",
+        },
+        closed_at: null,
+        draft: false,
         labels: ["labeled"],
-        number: 1,
-        title: 'test',
-        user: {
-          login: 'pr-creator',
-        },
       },
-      repository: {
-        name: 'label-when-approved-action',
-        owner: {
-          login: 'shin-shin-01',
-        },
+      review: {
+        state: "commented", // not approved
       },
     },
-    issue: {
-      owner: 'label-when-approved-action',
-      repo: 'shin-shin-01',
-      number: 1,
-    },
+    eventName: "pull_request_review",
+    actor: "Kaze-for-test",
   }
-  /* 
-  client = github.getOctokit(myToken) より client: undefined になる
-  addLabels: client.issues.addLabels ->  エラーが発生するため
-  モックとして作成し，Promise<void>を返すようにしておく
-  */
-  const addLabelsSpy = jest.spyOn(PullRequest.prototype, 'addLabels').mockReturnValue(Promise.resolve());
+  
+  const isApprovedSpy = jest.spyOn(PullRequest.prototype, 'isApproved')
+  const getUserNameSpy = jest.spyOn(PullRequest.prototype, 'getUserName')
   const hasAnyLabelSpy = jest.spyOn(PullRequest.prototype, 'hasAnyLabel')
+  const addLabelsSpy = jest.spyOn(PullRequest.prototype, 'addLabels').mockReturnValue(Promise.resolve());
 
   await main()
+  expect(isApprovedSpy).toHaveBeenCalled()
+  expect(getUserNameSpy).not.toHaveBeenCalled()
+  expect(hasAnyLabelSpy).not.toHaveBeenCalled()
+  expect(addLabelsSpy).not.toHaveBeenCalled()
+})
+
+test('failuer: already labeled by reviewer', async () => {
+  // @ts-ignore
+  github.context = {
+    payload: {
+      action: "submitted",
+      pull_request: {
+        assignee: null,
+        assignees: [],
+        auto_merge: null,
+        base: {
+          ref: "master",
+        },
+        closed_at: null,
+        draft: false,
+        labels: ["Kaze-for-test"], // already labeled
+      },
+      review: {
+        state: "approved",
+      },
+    },
+    eventName: "pull_request_review",
+    actor: "Kaze-for-test",
+  }
+  
+  const isApprovedSpy = jest.spyOn(PullRequest.prototype, 'isApproved')
+  const getUserNameSpy = jest.spyOn(PullRequest.prototype, 'getUserName')
+  const hasAnyLabelSpy = jest.spyOn(PullRequest.prototype, 'hasAnyLabel')
+  const addLabelsSpy = jest.spyOn(PullRequest.prototype, 'addLabels').mockReturnValue(Promise.resolve());
+
+  await main()
+  expect(isApprovedSpy).toHaveBeenCalled()
+  expect(getUserNameSpy).toHaveBeenCalled()
   expect(hasAnyLabelSpy).toHaveBeenCalled()
   expect(addLabelsSpy).not.toHaveBeenCalled()
+  // ユーザ名の確認
+ expect(new PullRequest('token', github.context).getUserName()).toBe("Kaze-for-test")
 })
